6eecd8c0b027da367f960258273c3ae89ffca603
Staffan Olsson <staffan@repos.se>
2020-06-21 11:37:20 +0200

    Digs out current native-image command args from Quarkus examples

---
+++ render.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+[ -z "$DEBUG" ] || set -x
+set -e
+
+EXAMPLES="
+kafka-quickstart
+"
+
+[ -d quarkus-quickstarts ] || git clone https://github.com/quarkusio/quarkus-quickstarts
+cd quarkus-quickstarts
+
+for EXAMPLE in $EXAMPLES; do
+  cd $EXAMPLE
+  printf "\nquarkus.native.additional-build-args=--dry-run\n" >> src/main/resources/application.properties
+  mvn -B package -Pnative > build.log || true
+  cat build.log | grep 'io.quarkus.deployment.pkg.steps.NativeImageBuildStep' | grep "$(which native-image)" | cut -d\  -f4- | sed 's/kafka-quickstart/__APP__/g' > ../../$EXAMPLE.txt
+done
+
+cd ..
